1c1b7398ba220d548dc36263804d953fe9da7da1
sakthipandu <sakthipandu32@gmail.com>
2024-04-23 13:53:46 +0530

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const dataRouter = require('./router');
-const pool = require('./db'); // Import the connection pool
 const cors = require('cors');
+const { Pool } = require('pg'); // Importing pg library to interact with PostgreSQL
+const dataRouter = require('./router');
+
 const app = express();
 const port = 5000;
 
-// Middleware
+// Connection string to PostgreSQL
+const postgres_url = "postgres://default:W5rvhOm0sJza@ep-quiet-bush-a489bw1a.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require";
+
+// Create a connection pool
+const pool = new Pool({
+  connectionString: postgres_url,
+});
+
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
-// CORS configuration
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://192.168.1.10:8080');      
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -19,16 +26,15 @@ app.use((req, res, next) => {
   next();
 });
 
-// Test database connection
-pool.query('SELECT NOW()', (err, res) => {
+// Use the connection pool to test database connectivity
+pool.query('SELECT NOW()', (err, dbRes) => {
   if (err) {
     console.error('Error connecting to PostgreSQL:', err);
   } else {
-    console.log('PostgreSQL connected:', res.rows);
+    console.log('PostgreSQL connected:', dbRes.rows);
   }
 });
 
-// Mount router
 app.use(dataRouter);
 
 // Test endpoint
